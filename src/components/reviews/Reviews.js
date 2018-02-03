615ded4e155a4fe291a073de55ccf070f4d3834f
Monika Williams <monika.m.williams@gmail.com>
2018-02-03 14:02:14 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Review from './Review';
 
-class Reviews extends Component { 
-  render() { 
-      const reviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId) 
+class Reviews extends Component {
+  render() {
+      const reviews = this.props.store.getState().reviews.filter(review => review.restaurantId === this.props.restaurantId)
       const filteredReviews = reviews.map((review, index) => {
         return <Review review={review} key={index} store={this.props.store} />
       });
-    
+
     return (
       <ul>
         {filteredReviews}
