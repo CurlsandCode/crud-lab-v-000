28ce200e480f8bbc07e2536b6dd1bb71784d5133
Monika Williams <monika.m.williams@gmail.com>
2018-02-03 14:01:45 +0000

    automatically backed up by learn

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
-  
+
   handleOnClick = () => {
     this.props.store.dispatch({
-      type: 'DELETE_REVIEW', 
+      type: 'DELETE_REVIEW',
       id: this.props.review.id,
     });
   }
-  
+
   render() {
     return (
       <li>
